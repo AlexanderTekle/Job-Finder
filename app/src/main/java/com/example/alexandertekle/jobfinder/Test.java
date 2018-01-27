94e97eb03e0fdd2c4b4b5387a9d1fefe1a7bcbad
Alexander Tekle <tekle.alexander@gmail.com>
2018-01-27 15:59:09 -0600

    Updates to toolbar

--- app/src/main/java/com/example/alexandertekle/jobfinder/Test.java
+++ app/src/main/java/com/example/alexandertekle/jobfinder/Test.java
@@ -2,12 +2,22 @@ package com.example.alexandertekle.jobfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class Test extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.mainactions, menu);
+        return true;
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
